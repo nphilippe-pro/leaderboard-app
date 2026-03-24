8151db728574a2878a1bbb7a7f97fe96ab14cae0
nphilippe-pro <nphilippe.pro@gmail.com>
2026-03-24 12:19:26 +0100

    Delete api directory

--- api/public-config.js
+++
@@ -1,6 +0,0 @@
-module.exports = async function handler(req, res) {
-  return res.status(200).json({
-    supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    supabaseAnonKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-  });
-};
